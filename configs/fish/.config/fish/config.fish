9026dfd2b5864cbbe2c8e21cefc7b00fd6a5b78e
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-01-09 21:53:47 +0000

    First Update 2025

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -84,3 +84,10 @@ set -gx PNPM_HOME "/home/paul/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/paul/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
